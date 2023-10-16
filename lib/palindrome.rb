b6f0e01bdcb2465357aef729c5c7aeaf2e5b2e78
koya-namba <koya.n.0603@gmail.com>
2023-10-16 18:13:27 +0900

    2ずつ増やした場合を追加

--- lib/palindrome.rb
+++ lib/palindrome.rb
@@ -1,3 +1,5 @@
+require 'benchmark'
+
 def palindrome(num)
   return false if num.to_s != num.to_s.reverse
   return false if num.to_s(2) != num.to_s(2).reverse
@@ -5,16 +7,36 @@ def palindrome(num)
   num
 end
 
-start_time = Time.now
-i = 10
-while true
-  result = palindrome(i)
-  if result
-    break
-  else
-    i += 1
+
+# 2ずつ増やした場合
+Benchmark.bm 100 do |x|
+  x.report("2ずつ増やした場合") do
+    i = 11
+    while true
+      result = palindrome(i)
+      if result
+        break
+      else
+        i += 2
+      end
+    end
+    puts "結果: #{result}"
   end
 end
 
-puts "結果: #{result}"
-puts "時間: #{Time.now - start_time}秒"
+
+# 1ずつ増やした場合
+Benchmark.bm 100 do |x|
+  x.report("1ずつ増やした場合") do
+    i = 10
+    while true
+      result = palindrome(i)
+      if result
+        break
+      else
+        i += 1
+      end
+    end
+    puts "結果: #{result}"
+  end
+end
